1cc1cf664c96daa5f9f7c4fee2800aaaffca41da
Diana Ramirez <dianitrix251017@gmail.com>
2022-03-01 22:05:32 -0600

    Evento click (Eventos en general)

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -2,10 +2,19 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 
 const CounterApp = ({ value }) => {
+
+    // handleAdd
+    const handleAdd = (e) => {
+        console.log(e)
+    }
+
+
     return (
         <>
             <h1>CounterApp</h1>
             <h2> { value } </h2>
+
+            <button onClick={ handleAdd }>+1</button>
         </>
     )
 }
