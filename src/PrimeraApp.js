536cd733a7961d9df67c990e24b64d710cbafd2b
Diana Ramirez <dianitrix251017@gmail.com>
2022-03-01 20:57:25 -0600

    Retornar elementos en el Componente - Fragment

--- src/PrimeraApp.js
+++ src/PrimeraApp.js
@@ -1,9 +1,16 @@
 import React from 'react';
+// import React, { Fragment } from 'react';
 
 const PrimeraApp = () => {
 
     
-    return <h1>Hola Mundo</h1>;
+    return (
+        <>
+            <h1>Hola Mundo</h1>
+            <p>Mi primera aplicación</p>
+        </>
+    );
+    
 
 } 
 
